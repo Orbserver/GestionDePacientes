3488dd07c9f72933648be4aa2304307b0ca01b52
Orbserver <tiberiumbass@gmail.com>
2023-10-29 16:22:37 +0100

    probando peticiones HTTP

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -19,7 +19,7 @@ class PacienteController extends Controller
     }
 
     //Procesamiento de consulta y presentacion de los datos
-    public function consultarPaciente(Request $request) {
+    public function resultadosConsulta(Request $request) {
         $request->validate([
             'nombre' => 'required_without_all:primer_apellido,segundo_apellido,fecha_nacimiento,telefono|string|max:255',
             'primer_apellido' => 'required_without_all:nombre,segundo_apellido,fecha_nacimiento,telefono|string|max:255',
@@ -28,8 +28,32 @@ class PacienteController extends Controller
             'telefono' => 'required_without_all:,nombre,primer_apellido,segundo_apellido,fecha_nacimiento|string|max:20',
         ]);
         //Logica para procesar la consulta y mostrar resultados
+        $query = Paciente::query();
+
+        if ($request->has('nombre')) {
+            $query->where('nombre', 'like', '%' . $request->input('nombre') . '%');
+        }
+
+        if ($request->has('primer_apellido')) {
+            $query->where('primer_apellido', 'like', '%' . $request->input('primer_apellido') . '%');
+        }
+
+        if ($request->has('segundo_apellido')) {
+            $query->where('segundo_apellido', 'like', '%' . $request->input('segundo_apellido') . '%');
+        }
+
+        if ($request->has('fecha_nacimiento')) {
+            $query->where('fecha_nacimiento', 'like', '%' . $request->input('fecha_nacimiento') . '%');
+        }
+
+        if ($request->has('telefono')) {
+            $query->where('telefono', 'like', '%' . $request->input('telefono') . '%');
+        }
+
+        $resultados = $query->get();
+
         //Se puede acceder a los datos del formulario usando $request
-        return view('resultados_consulta');
+        return view('resultados_consulta', ['resultados' => $resultados]);
     }
 
     //Formulario de nuevo paciente
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\PacienteController;
 
 Route::get('/', [PacienteController::class, 'index']); //Pagina principal
 Route::get('/consulta-paciente', [PacienteController::class, 'consultaPaciente']); //Formulario consulta de paciente
-Route::post('/consulta-paciente', [PacienteController::class, 'consultarPaciente']); //Procesar consulta
+Route::post('/resultados-consulta', [PacienteController::class, 'resultadosConsulta']); //Procesar consulta
 Route::get('/nuevo-paciente', [PacienteController::class, 'nuevoPaciente']); //Formulario de creación
 Route::post('/nuevo-paciente', [PacienteController::class, 'guardarNuevoPaciente']); //Procesar creación 
